3043e852c58301376472d027da6c36b8306f3e24
vyolete <44974105+vyolete@users.noreply.github.com>
2025-10-06 10:03:24 -0500

    Update app.py
    actualizacion imagen de eventos

--- app.py
+++ app.py
@@ -366,7 +366,7 @@ def modulo_eventos():
     col1, col2 = st.columns([1, 2])
 
     with col1:
-        st.image("https://imgur.com/a/UrDYzdd", use_column_width=True)
+        st.image("https://imgur.com/a/UrDYzdd", use_container_width=True)
 
     with col2:
         st.markdown("""
